40fd7de429b6e93b5a259baa35e88125549614a5
wenpliu <wen.p.liu@gmail.com>
2017-08-05 22:51:44 -0700

    adjusted delta, a scaled to internal to consider latency especially when turning corners.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -118,10 +118,17 @@ public:
             AD<double> delta0 = vars[delta_start + t - 1];
             AD<double> a0 = vars[a_start + t - 1];
 
-            // use previous actuations (to account for latency)
-            if (t > 1) {
-                a0 = vars[a_start + t - 2];
-                delta0 = vars[delta_start + t - 2];
+            // adjust delta, a for latency
+            if (t > 1)
+            {
+                    if(t < 5) {
+                        a0 = vars[a_start + t - 2];
+                        delta0 = vars[delta_start + t - 2];
+                    }else
+                    {
+                        a0 = vars[a_start + t - 3];
+                        delta0 = vars[delta_start + t - 3];
+                    }
             }
             AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
             AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
