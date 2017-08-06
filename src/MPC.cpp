c8d8da0f0c19d6d83226672413a29088af0a135c
wenpliu <wen.p.liu@gmail.com>
2017-08-05 20:20:54 -0700

    changed order of delta and a.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -287,13 +287,13 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     // creates a 2 element double vector.
     vector<double> result;
 
+    result.push_back(solution.x[delta_start]);
+    result.push_back(solution.x[a_start]);
+
     for (int i = 0; i < N-1; i++) {
         result.push_back(solution.x[x_start + i + 1]);
         result.push_back(solution.x[y_start + i + 1]);
     }
-
-    result.push_back(solution.x[delta_start]);
-    result.push_back(solution.x[a_start]);
 
     return result;
 }
