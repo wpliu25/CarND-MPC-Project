204f3dc59894813ff665777b8e5b9f5bd748ae95
wenpliu <wen.p.liu@gmail.com>
2017-08-05 19:31:30 -0700

    updated MCP::Solve from quiz. Car with forward motion but UI incorrect. Still runs off.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -160,11 +160,13 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     size_t i;
     typedef CPPAD_TESTVECTOR(double) Dvector;
 
-    // TODO: Set the number of model variables (includes both states and inputs).
-    // For example: If the state is a 4 element vector, the actuators is a 2
-    // element vector and there are 10 timesteps. The number of variables is:
-    //
-    // 4 * 10 + 2 * 9
+    double x = state[0];
+    double y = state[1];
+    double psi = state[2];
+    double v = state[3];
+    double cte = state[4];
+    double epsi = state[5];
+
     // number of independent variables
     // N timesteps == N - 1 actuations
     size_t n_vars = N * 6 + (N - 1) * 2;
@@ -172,45 +174,67 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     size_t n_constraints = N * 6;
 
     // Initial value of the independent variables.
-    // SHOULD BE 0 besides initial state.
+    // Should be 0 except for the initial values.
     Dvector vars(n_vars);
     for (int i = 0; i < n_vars; i++) {
-        vars[i] = 0;
+      vars[i] = 0.0;
     }
+    // Set the initial variable values
+    vars[x_start] = x;
+    vars[y_start] = y;
+    vars[psi_start] = psi;
+    vars[v_start] = v;
+    vars[cte_start] = cte;
+    vars[epsi_start] = epsi;
 
+    // Lower and upper limits for x
     Dvector vars_lowerbound(n_vars);
     Dvector vars_upperbound(n_vars);
+
     // Set all non-actuators upper and lowerlimits
     // to the max negative and positive values.
     for (int i = 0; i < delta_start; i++) {
-        vars_lowerbound[i] = -1.0e19;
-        vars_upperbound[i] = 1.0e19;
+      vars_lowerbound[i] = -1.0e19;
+      vars_upperbound[i] = 1.0e19;
     }
 
     // The upper and lower limits of delta are set to -25 and 25
     // degrees (values in radians).
     // NOTE: Feel free to change this to something else.
     for (int i = delta_start; i < a_start; i++) {
-        vars_lowerbound[i] = -0.436332;
-        vars_upperbound[i] = 0.436332;
+      vars_lowerbound[i] = -0.436332;
+      vars_upperbound[i] = 0.436332;
     }
 
     // Acceleration/decceleration upper and lower limits.
     // NOTE: Feel free to change this to something else.
     for (int i = a_start; i < n_vars; i++) {
-        vars_lowerbound[i] = -1.0;
-        vars_upperbound[i] = 1.0;
+      vars_lowerbound[i] = -1.0;
+      vars_upperbound[i] = 1.0;
     }
 
-
-    // Lower and upper limits for the constraints
-    // Should be 0 besides initial state.
+    // Lower and upper limits for constraints
+    // All of these should be 0 except the initial
+    // state indices.
     Dvector constraints_lowerbound(n_constraints);
     Dvector constraints_upperbound(n_constraints);
     for (int i = 0; i < n_constraints; i++) {
-        constraints_lowerbound[i] = 0;
-        constraints_upperbound[i] = 0;
+      constraints_lowerbound[i] = 0;
+      constraints_upperbound[i] = 0;
     }
+    constraints_lowerbound[x_start] = x;
+    constraints_lowerbound[y_start] = y;
+    constraints_lowerbound[psi_start] = psi;
+    constraints_lowerbound[v_start] = v;
+    constraints_lowerbound[cte_start] = cte;
+    constraints_lowerbound[epsi_start] = epsi;
+
+    constraints_upperbound[x_start] = x;
+    constraints_upperbound[y_start] = y;
+    constraints_upperbound[psi_start] = psi;
+    constraints_upperbound[v_start] = v;
+    constraints_upperbound[cte_start] = cte;
+    constraints_upperbound[epsi_start] = epsi;
 
     // object that computes objective and constraints
     FG_eval fg_eval(coeffs);
